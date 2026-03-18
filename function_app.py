320158db5ef7511b46a1c718aca7edbaa030cfed
Peko <31332610+pekosong@users.noreply.github.com>
2026-03-18 11:22:29 +0900

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -4,4 +4,4 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="vdr")
 def vdr_api(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("Hello from Azure Functions!", status_code=200)
+    return func.HttpResponse("Hello from Azure Functions 입니다.", status_code=200)
